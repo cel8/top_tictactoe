584bde7a58c579f04c2b531a5b4078faf1e20873
cel8 <alex.celotti@gmail.com>
2022-07-07 14:44:04 -0400

    Add some style but not working on mobile devices

--- script/displayController.js
+++ script/displayController.js
@@ -41,32 +41,40 @@ export class DisplayController {
   loadLeftPlayer(playerName, icon) {
     const img = boardPlayerA.querySelector('.icon');
     const name = boardPlayerA.querySelector('.playerName');
+    const scoreContainer = boardPlayerA.querySelector('.scoreContainer');
     const score = boardPlayerA.querySelector('.score');
     img.setAttribute('src', svgPath + icon);
     name.textContent = playerName;
+    scoreContainer.style.display = 'block';
     score.textContent = '0';
   }
   loadRightPlayer(playerName, icon) {
     const img = boardPlayerB.querySelector('.icon');
     const name = boardPlayerB.querySelector('.playerName');
+    const scoreContainer = boardPlayerB.querySelector('.scoreContainer');
     const score = boardPlayerB.querySelector('.score');
     img.setAttribute('src', svgPath + icon);
     name.textContent = playerName;
+    scoreContainer.style.display = 'block';
     score.textContent = '0';
   }
   unloadLeftPlayer() {
     const img = boardPlayerA.querySelector('.icon');
     const name = boardPlayerA.querySelector('.playerName');
+    const scoreContainer = boardPlayerA.querySelector('.scoreContainer');
     const score = boardPlayerA.querySelector('.score');
     img.removeAttribute('src');
     name.textContent = '';
+    scoreContainer.style.display = 'none';
     score.textContent = '';
   }
   unloadRightPlayer() {
     const img = boardPlayerB.querySelector('.icon');
     const name = boardPlayerB.querySelector('.playerName');
+    const scoreContainer = boardPlayerB.querySelector('.scoreContainer');
     const score = boardPlayerB.querySelector('.score');
     img.removeAttribute('src');
+    scoreContainer.style.display = 'none';
     name.textContent = '';
     score.textContent = '';
   }
@@ -87,10 +95,10 @@ export class DisplayController {
       const svgName = (winnerPlayer.type === 'bot' ? 'robot-love-outline.svg' 
                                                    : 'account-heart-outline.svg');
       img.setAttribute('src', svgPath + svgName);
-      text.textContent = `The winner is ${winnerPlayer.name}.`;
+      text.textContent = `The winner is ${winnerPlayer.name}`;
     } else {
       img.removeAttribute('src');
-      text.textContent = `It's a tie.`
+      text.textContent = `It's a tie`
     }
   }
   setSlot(x, y, side) {
@@ -109,14 +117,14 @@ export class DisplayController {
                                     : 'account-outline.svg');
     }
     if(this.playerA.side === 'X') {
-      const leftSvgName = getIconName(this.playerA.name);
-      const rightSvgName = getIconName(this.playerB.name);
+      const leftSvgName = getIconName(this.playerA);
+      const rightSvgName = getIconName(this.playerB);
       this.loadLeftPlayer(this.playerA.name, leftSvgName);
       this.loadRightPlayer(this.playerB.name, rightSvgName);
       this.currentPlayer = this.playerA;
     } else {
-      const leftSvgName = getIconName(this.playerB.name);
-      const rightSvgName = getIconName(this.playerA.name);
+      const leftSvgName = getIconName(this.playerB);
+      const rightSvgName = getIconName(this.playerA);
       this.loadLeftPlayer(this.playerB.name, leftSvgName);
       this.loadRightPlayer(this.playerA.name, rightSvgName);
       this.currentPlayer = this.playerB;
@@ -127,7 +135,7 @@ export class DisplayController {
     return this.currentPlayer;
   }
   loadPlayerRound(playerName, playerSide) {
-    divUserRound.textContent = `${playerName}: ${playerSide} round.`;
+    divUserRound.textContent = `${playerName}'s ${playerSide} round`;
   }
   unloadPlayers() {
     divUserRound.textContent = '';
